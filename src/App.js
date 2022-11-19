76927201f9e5a0a7c16d7c3e2e7903dea60525a6
paullaster <paullasterokoth98@gmail.com>
2022-11-19 22:20:12 +0300

    adding select state mgnt

--- src/App.js
+++ src/App.js
@@ -7,15 +7,32 @@ import Statistics from './components/Statistics';
 import Table from './components/Table';
 
 
-
-
 const App = () => {
   const [statistics, setStatistics] = useState([]);
-  const [country, SetCountry] = useState(['kenya', 'uganda', 'tanzania',]);
+  const [countryList, setCountryList] = useState([]);
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  useEffect ( () => {
-    setIsLoading (true);
+
+  const FetchPreferredCountryList = () => {
+    fetch ('https://covid-193.p.rapidapi.com/countries', {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+      }
+    })
+    .then ( (response) => {
+      return response.json ();
+    })
+    .then ( (data) => {
+      setCountryList (data.response);
+      setIsLoading (false);
+    })
+    .catch ( (err) => {
+      setIsError (true);
+    } );
+  };
+
+  const FetchStatistics = () => {
     fetch ( 'https://covid-193.p.rapidapi.com/statistics', {
       method: 'GET',
       headers: {
@@ -32,13 +49,22 @@ const App = () => {
     .catch ( () => {
       setIsError ( true);
     })
+
+  };
+
+
+
+  useEffect ( () => {
+    setIsLoading (true);
+    FetchStatistics ();
+    FetchPreferredCountryList ();
   }, [])
   return (
     <>
     <Error error={isError}/>
     { isLoading && <p> Loading...</p>}
     <Form>
-      <Select countryList={country}/>
+      <Select countryList={countryList}/>
     </Form>
      <Statistics>
        <Table statistics={statistics}/>
